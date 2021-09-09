83c58032286326b2bcf6f883d7d5af9d635d4580
nestorivanmo <nestor.martinez.98@hotmail.com>
2021-09-09 13:39:19 -0500

    ...

--- spring-21/client-server-architecture/csa-project/src/client.c
+++ spring-21/client-server-architecture/csa-project/src/client.c
@@ -8,67 +8,84 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-// the port client will be connecting to
 #define PORT 3490
-// max number of bytes we can get at once
 #define MAXDATASIZE 300
+
+void print_usage_help(char *argv[]){
+  printf("----------------------\n\n");
+  printf("Usage: \n------\n\t%s <client_ip_addr> <flags> <SQL commands>\n", argv[0]);
+  printf("\nSQL commands: \n-------------\n");
+  printf("\t1) insert <num_cta> <first_last_name> <second_last_name> <name>\n");
+  printf("\t2) select <num_cta>\n");
+  printf("\t3) exit\n");
+  printf("\n----------------------\n");
+}
+
+void verify_cli_args(int argc, char *argv[]) {
+  if (argc == 1) {
+    print_usage_help(argv);
+    exit(1);
+  }
+}
+
 
 int main(int argc, char *argv[]){
   int sockfd, numbytes;
   char buf[MAXDATASIZE];
   struct hostent *he;
-
-  // connectors address information
   struct sockaddr_in their_addr;
-  // if no command line argument supplied
-  if(argc != 2){
-    fprintf(stderr, "Client-Usage: %s host_servidor\n", argv[0]);
+  
+  verify_cli_args(argc, argv);
 
-    // just exit
+  //-----------CLI PARSING--------------------
+  char *ip_addr = argv[1];
+
+
+  //-------------CONFIGURATION----------------
+  if((he=gethostbyname(ip_addr)) == NULL){
+    printf("CLIENT: error while getting host info: %s\n", ip_addr);
     exit(1);
   }
-
-  // get the host info
-  if((he=gethostbyname(argv[1])) == NULL){
-    perror("gethostbyname()");
-    exit(1);
-  }
-  else
-    printf("Client-The remote host is: %s\n", argv[1]);
-
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
     perror("socket()");
     exit(1);
   }
-  else 
-    printf("Client-The socket() sockfd is OK...\n");
-
-  // host byte order
   their_addr.sin_family = AF_INET;
-  // short, network byte order
-  printf("Server-Using %s and port %d...\n", argv[1], PORT);
   their_addr.sin_port = htons(PORT);
   their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-
-  // zero the rest of the struct
   memset(&(their_addr.sin_zero), '\0', 8);
+
+  //-------------CONNECTION----------------
   if(connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1){
     perror("connect()");
     exit(1);
   }
-  else
-    printf("Client-The connect() is OK...\n");
+
+  //---------------CLIENT REQUESTS-----------------
+  char client_command[1000];
+  printf("Command > ");
+  scanf("%s", client_command);
+
+  while (strcmp(client_command, "exit") != 0) {
+
+    if (send(sockfd, client_command, strlen(client_command), 0) == -1) {
+      printf("CLIENT: could not send message to server");
+    }
+
+    printf("Command > ");
+    scanf("%s", client_command);
+  }
+
+  /*
 
   if((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1){
     perror("recv()");
     exit(1);
   }
-  else
-    printf("Client-The recv() is OK...\n");
 
   buf[numbytes] = '\0';
-  printf("Client-Received: %s", buf);
-  printf("Client-Closing sockfd\n");
+  printf("CLIENT: Received: %s", buf);
+  printf("CLIENT: Closing sockfd\n"); */
   close(sockfd);
   return 0;
 }
